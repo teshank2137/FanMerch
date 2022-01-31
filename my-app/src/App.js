2327566eae64cbd61cd065b05e6a67bd73fac667
Teshank Raut <teshank.2137@gmail.com>
2022-01-31 09:45:33 +0530

    added code splliting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
+import { lazy, Suspense } from "react";
 import styled, { keyframes } from "styled-components";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import NavBar from "./components/Nanbar";
-import Cart from "./pages/Cart";
-import Details from "./pages/Details";
-import Login from "./pages/login";
-import Shop from "./pages/Shop";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import Checkout from "./pages/Checkout";
-import Profile from "./pages/Profile";
+import Loading from "./components/Loding";
+const Cart = lazy(() => import("./pages/Cart"));
+const Details = lazy(() => import("./pages/Details"));
+const Login = lazy(() => import("./pages/login"));
+const Shop = lazy(() => import("./pages/Shop"));
+const Signup = lazy(() => import("./pages/Signup"));
+const Home = lazy(() => import("./pages/Home"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const Profile = lazy(() => import("./pages/Profile"));
 
 const gradient = keyframes`
 0% {
@@ -42,16 +44,18 @@ function App() {
         <div className="back"></div>
         <Router>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/account/login" element={<Login />} />
-            <Route path="/account/signup" element={<Signup />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/account/login" element={<Login />} />
+              <Route path="/account/signup" element={<Signup />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </Suspense>
         </Router>
       </StyledBackground>
     </Provider>
